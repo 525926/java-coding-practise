1c09a688ef61af9dfb13c85d1ac34f72c232c7f7
ashar <ashar@LAPTOP-4V70B73B>
2022-09-22 18:45:06 +0530

    addition

--- Java Programs/src/Factorial/Add.java
+++ Java Programs/src/Factorial/Add.java
@@ -3,7 +3,9 @@ package Factorial;
 public class Add {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
 
 	}
 
